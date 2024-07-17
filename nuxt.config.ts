7fab56e3e643f5d21c509d63646ae0e121352dd4
Prosper Ugbovo [YONGDEV] <85100001+prosperyong@users.noreply.github.com>
2024-07-17 10:58:08 +0100

    :construction: chore: add shadcn/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxt/content"]
+  modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "shadcn-nuxt"]
 })
